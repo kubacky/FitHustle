a6c6c90c271d28a5479aef1a995c86e928890b3d
kubacky <kubacki.artur@gmail.com>
2021-03-27 23:33:37 +0100

    fix :current_confirmation error in the Users::RegistrationsController

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :name, :surname, :password, :current_confirmation, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :name, :surname, :password, :current_password, :role])
   end
 
   def resolve_layout
